a005ffde86b3a79316b6d38105c733e405dc70f2
valiant1x <29789261+valiant1x@users.noreply.github.com>
2019-01-02 07:18:36 +0000

    use local docker build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,10 @@ GITHUB_CLIENT_SECRET=$3
 
 #ssh -oStrictHostKeyChecking=no ${SSH_USER}@${SSH_HOST} "PKG_VERSION=$PKG_VERSION CURRENCY=$CURRENCY GITHUB_CLIENT_ID=$GITHUB_CLIENT_ID GITHUB_CLIENT_SECRET=$GITHUB_CLIENT_SECRET bash -s" << 'EOF'
 
-IMAGE_NAME="letheanmovement/lethean-pool-list:$PKG_VERSION"
-CONTAINER_NAME="$CURRENCY-pool-list"
-VIRTUAL_HOST="pools.lethean.io,$CURRENCY-pools.containers"
-VIRTUAL_PORT=80
+IMAGE_NAME="lethean-pool-list:latest"
+CONTAINER_NAME="lethean-pool-list"
+VIRTUAL_HOST="pools.lethean.io"
+VIRTUAL_PORT=8888
 LETSENCRYPT_HOST="pools.lethean.io"
 LETSENCRYPT_EMAIL="valiant@lethean.io"
 BASE_URL="https://$LETSENCRYPT_HOST"
@@ -26,9 +26,9 @@ echo "VIRTUAL_PORT: $VIRTUAL_PORT"
 echo "LETSENCRYPT_HOST: $LETSENCRYPT_HOST"
 echo "LETSENCRYPT_EMAIL: $LETSENCRYPT_EMAIL"
 
-docker pull ${IMAGE_NAME}
+#docker pull ${IMAGE_NAME}
 docker rm -f ${CONTAINER_NAME}
-docker run -d --network=containers --restart always --name ${CONTAINER_NAME} \
+docker run -d --restart always --name ${CONTAINER_NAME} \
     -e CURRENCY=${CURRENCY} \
     -e ENV_NAME=${CONTAINER_NAME} \
     -e VIRTUAL_HOST=${VIRTUAL_HOST} \
